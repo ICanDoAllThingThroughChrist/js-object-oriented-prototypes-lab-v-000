2bda12048647b56103fd3fdf13feef694e6aa949
charlie  lee <ctlee9096@gmail.com>
2018-12-26 04:13:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function BoardMember(name, homeState, training){
 }
 BoardMember.prototype.veto = function(){
     return 'No, I must disagree'
-}
+};
   this.approve = function(){
     return 'You can do that!'
   }
