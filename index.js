0b141d8fcbd20f460c768b04fd9791790b1f905d
charlie  lee <ctlee9096@gmail.com>
2018-12-26 04:14:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ BoardMember.prototype.doCharity = function(){
     return "I like to help people."
   }
 
-  this.releasePressStatement = function(){
+BoardMember.prototype.releasePressStatement = function(){
     return "You will see great things from Scuber."
   }
 
